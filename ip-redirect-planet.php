122d99f77411eb0bc877529dfac4ec76226f6fb2
Reloaded <deepak@designandcode.tech>
2023-06-01 15:40:23 +0530

    final

--- ip-redirect-planet.php
+++ ip-redirect-planet.php
@@ -28,14 +28,22 @@ add_action('admin_menu', 'location_redirect_settings_menu');
 function location_redirect_settings_page() {
   // Save settings if the form is submitted
   if (isset($_POST['submit'])) {
-      update_option('location_redirect_location', $_POST['location']);
-      update_option('location_redirect_url', $_POST['url']);
-      echo '<div class="notice notice-success"><p>Settings saved.</p></div>';
-  }
+    update_option('location_redirect_location_1', $_POST['location_1']);
+    update_option('location_redirect_url_1', $_POST['url_1']);
+    update_option('location_redirect_location_2', $_POST['location_2']);
+    update_option('location_redirect_url_2', $_POST['url_2']);
+    update_option('location_redirect_location_3', $_POST['location_3']);
+    update_option('location_redirect_url_3', $_POST['url_3']);
+    echo '<div class="notice notice-success"><p>Settings saved.</p></div>';
+}
 
   // Retrieve saved settings
-  $location = get_option('location_redirect_location');
-  $url = get_option('location_redirect_url');
+  $location_1 = get_option('location_redirect_location_1');
+  $url_1 = get_option('location_redirect_url_1');
+  $location_2 = get_option('location_redirect_location_2');
+  $url_2 = get_option('location_redirect_url_2');
+  $location_3 = get_option('location_redirect_location_3');
+  $url_3 = get_option('location_redirect_url_3');
   ?>
 
   <div class="wrap">
@@ -43,18 +51,36 @@ function location_redirect_settings_page() {
 
       <form method="post" action="">
           <table class="form-table">
-              <tr>
-                  <th scope="row"><label for="location">Location</label></th>
-                  <td>
-                      <input type="text" name="location" id="location" value="<?php echo esc_attr($location); ?>" />
-                  </td>
-              </tr>
-              <tr>
-                  <th scope="row"><label for="url">Redirect URL</label></th>
-                  <td>
-                      <input type="text" name="url" id="url" value="<?php echo esc_attr($url); ?>" />
-                  </td>
-              </tr>
+          <tr>
+                    <th scope="row"><label for="location_1">Location 1</label></th>
+                    <td>
+                        <input type="text" name="location_1" id="location_1" value="<?php echo esc_attr($location_1); ?>" />
+                    </td>
+                    <th scope="row"><label for="url_1">Redirect URL 1</label></th>
+                    <td>
+                        <input type="text" name="url_1" id="url_1" value="<?php echo esc_attr($url_1); ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="location_2">Location 2</label></th>
+                    <td>
+                        <input type="text" name="location_2" id="location_2" value="<?php echo esc_attr($location_2); ?>" />
+                    </td>
+                    <th scope="row"><label for="url_2">Redirect URL 2</label></th>
+                    <td>
+                        <input type="text" name="url_2" id="url_2" value="<?php echo esc_attr($url_2); ?>" />
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="location_3">Location 3</label></th>
+                    <td>
+                        <input type="text" name="location_3" id="location_3" value="<?php echo esc_attr($location_3); ?>" />
+                    </td>
+                    <th scope="row"><label for="url_3">Redirect URL 3</label></th>
+                    <td>
+                        <input type="text" name="url_3" id="url_3" value="<?php echo esc_attr($url_3); ?>" />
+                    </td>
+                </tr>
           </table>
 
           <p class="submit">
@@ -68,8 +94,12 @@ function location_redirect_settings_page() {
 
 // Redirect users based on their location
 function location_redirect() {
-  $location = get_option('location_redirect_location');
-  $url = get_option('location_redirect_url');
+  $location_1 = get_option('location_redirect_location_1');
+  $url_1 = get_option('location_redirect_url_1');
+  $location_2 = get_option('location_redirect_location_2');
+  $url_2 = get_option('location_redirect_url_2');
+  $location_3 = get_option('location_redirect_location_3');
+  $url_3 = get_option('location_redirect_url_3');
   
   // Get the user's IP address
   $ip = $_SERVER['REMOTE_ADDR'];
@@ -78,14 +108,34 @@ function location_redirect() {
 
   $api_url = "https://ipinfo.io/$ip/json";
   $response = wp_remote_get($api_url);
-  if (!is_wp_error($response)) {
+
+  if (!is_wp_error($response) && $_SERVER['REQUEST_URI']==='/'  && !isset($_COOKIE['redirected'])) {
       $body = wp_remote_retrieve_body($response);
       $data = json_decode($body);
-      // Check if the user's location matches the configured location
-      if ($data->country === $location && is_front_page()) {
-          wp_redirect($url);
-          exit;
-      }
+      // Check if the user's location matches the configured location        
+        
+      setcookie("redirected", true, time()+1296000, COOKIEPATH, COOKIE_DOMAIN, true, true );
+
+        switch ($data->country) {
+            case $location_1:
+                wp_redirect($url_1);
+                 exit;
+                break;
+            
+            case $location_2:
+                    wp_redirect($url_2);
+                     exit;
+                    break;
+             case $location_2:
+                wp_redirect($url_2);
+                   exit;
+                   break;
+            
+            default:
+                return;
+                break;
+        }
+         
   }
 }
 add_action('template_redirect', 'location_redirect');
